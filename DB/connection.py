5a993009a7a17638cc1e84e86eb7e57f218fd2b1
Darius Lacatusu 1090 <lacatusudariusnicolae@gmail.com>
2026-07-06 00:32:57 +0300

    Un commit 1

--- DB/connection.py
+++ DB/connection.py
@@ -1,20 +1,51 @@
+# from sqlalchemy import create_engine
+# from dotenv import load_dotenv
+# import os
+
+# load_dotenv()
+
+# def get_engine():
+#     url = (
+#         f"postgresql+psycopg2://"
+#         f"{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
+#         f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}"
+#         f"/{os.getenv('DB_NAME')}"
+#     )
+
+#     return create_engine(url)
+
+# if __name__=="__main__":
+#     engine=get_engine()
+#     with engine.connect() as conn:
+#         print("Conectat in mod reusit")
+
+
 from sqlalchemy import create_engine
-from dotenv import load_dotenv
+import streamlit as st
 import os
+from dotenv import load_dotenv
 
 load_dotenv()
 
 def get_engine():
+    def get_db_cred(key):
+        if key in st.secrets:
+            return st.secrets[key]
+        return os.getenv(key)
+
     url = (
         f"postgresql+psycopg2://"
-        f"{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
-        f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}"
-        f"/{os.getenv('DB_NAME')}"
+        f"{get_db_cred('DB_USER')}:{get_db_cred('DB_PASSWORD')}"
+        f"@{get_db_cred('DB_HOST')}:{get_db_cred('DB_PORT')}"
+        f"/{get_db_cred('DB_NAME')}"
     )
+    connect_args = {}
+    if "neon.tech" in get_db_cred('DB_HOST'):
+        connect_args = {"sslmode": "require"}
 
-    return create_engine(url)
+    return create_engine(url, connect_args=connect_args)
 
 if __name__=="__main__":
-    engine=get_engine()
+    engine = get_engine()
     with engine.connect() as conn:
         print("Conectat in mod reusit")
